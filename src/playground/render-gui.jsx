02443b5ee60a2efe4c8a90273f0a51cfd4ddaf1e
dev-palboksoft <dev@palboksoft.com>
2020-12-26 09:33:11 +0900

    수정

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -8,7 +8,7 @@ import HashParserHOC from '../lib/hash-parser-hoc.jsx';
 import log from '../lib/log.js';
 
 const onClickLogo = () => {
-    window.location = '/';
+    window.location = 'javascript: location.reload()';
 };
 
 const handleTelemetryModalCancel = () => {
